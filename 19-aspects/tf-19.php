0c0781ab6365d60e0a370030b8e8c289bd74bb19
kimsikkong <kimsikkong@gmail.com>
2021-02-24 22:13:10 +0900

    feat: 19-aspects php 추가 - 2
    - profile wrapper 추가

--- 19-aspects/tf-19.php
+++ 19-aspects/tf-19.php
@@ -6,6 +6,7 @@ function extractWord()
       $word_str = fread(fopen($file_path, 'r'), filesize($file_path));
       $words = explode(" ", mb_strtolower(preg_replace("/[\W_]+/", " ", $word_str)));
       $stop_words = explode(",", fgets(fopen("../stop_words.txt", 'r')));
+      for ($i = 97; $i < 123; $i++) { $stop_words[] = chr($i); }
       return array_filter($words, function($word) use ($stop_words) {
           if (!in_array($word, $stop_words)) {
               return $word;
@@ -40,6 +41,27 @@ function _sort()
     };
 }
 
-$tracked_functions = [extractWord(), frequency(), _sort()];
+function profileWrapper(callable $f)
+{
+    return function($args) use ($f) {
+      $start_time = get_time();
+      $ret = $f($args);
+      $end_time = get_time();
+      $time = $end_time - $start_time;
+      echo number_format($time, 6) . " 초 걸림" . PHP_EOL;
+      return $ret;
+    };
+}
 
-// TODO 
+function get_time() { $t=explode(' ',microtime()); return (float)$t[0]+(float)$t[1]; }
+
+$f1 = profileWrapper(extractWord());
+$f2 = profileWrapper(frequency());
+$f3 = profileWrapper(_sort());
+
+$word_freq = $f3($f2($f1($argv[1])));
+$word_freq = array_slice($word_freq, 0, 25);
+
+foreach ($word_freq as $k => $v) {
+    echo "$k - $v" . PHP_EOL;
+}
